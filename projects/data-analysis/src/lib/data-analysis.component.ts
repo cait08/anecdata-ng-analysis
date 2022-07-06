465521dfdc907139b48dedd1121861578b878847
Cait Bailey <cait@mdibl.org>
2022-07-06 13:18:30 -0400

    update

---
+++ projects/data-analysis/src/lib/data-analysis.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { ObservationApiService } from 'anecdata-ngcore';
+import { map, Observable, Subject } from 'rxjs';
+@Component({
+  selector: 'lib-data-analysis',
+  template: `
+    <p>
+      data-analysis works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DataAnalysisComponent  {
+
+
+  posts: Observable<any[]> = new Subject();
+
+  constructor(
+    private obsApi: ObservationApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.posts = this.obsApi.list({}).pipe(map(res => res.posts));
+  }
+
+}
--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,20 +1,29 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ObservationApiService } from 'anecdata-ngcore';
+import { DataAnalysisModule } from 'projects/data-analysis/src/public-api';
 import { StarterLibraryModule } from 'projects/starter-library/src/public-api';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AnecdataCoreModule } from 'anecdata-ngcore';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    HttpClientModule,
+    AnecdataCoreModule.forRoot({
+      apiKey: 'blerg',
+      baseUrl: 'https://www.anecdata.org',
+      appName: 'data-analysis-demo',
+      appVersion: '1.0.0',
+    }),
+    DataAnalysisModule,
     BrowserModule,
     AppRoutingModule,
-    StarterLibraryModule
+    StarterLibraryModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [HttpClient, ObservationApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
